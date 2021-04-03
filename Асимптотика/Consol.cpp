76ef8e55f6e8648b63778ed55dd96e1368dabea7
YankhotovDenny <65371997+YankhotovDenny@users.noreply.github.com>
2021-04-03 12:29:22 +0300

    Add files via upload

---
+++ Асимптотика/Consol.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <list>
+
+
+
+int main() {
+	std::list<short int> head;
+	std::vector<short int> head1;
+	std::ofstream out;
+	out.open("D:\\h.txt");
+	if (out.is_open()) out << "edasd" << std::endl;
+	for (int i = 1; i < 1000000000; i++)
+	{
+		out << i << std::endl;
+	}
+}
