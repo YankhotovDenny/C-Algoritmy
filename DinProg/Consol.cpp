f45e04535ea32c45e71d3dcee34a07e35fdf5096
YankhotovDenny <65371997+YankhotovDenny@users.noreply.github.com>
2021-04-28 14:46:27 +0300

    Update Consol.cpp

--- DinProg/Consol.cpp
+++ DinProg/Consol.cpp
@@ -6,14 +6,14 @@ std::vector<int> Rykzak(int n, std::vector<int> weight, std::vector<int> value, 
 	int a = 0;
 	std::list<int> h;
 	for (int i = 0; i < qu; ++i) {
-		if (n - weight[i]>= 0) {
+		if (n - weight[i] >= 0) {
 			a = head[n - weight[i]] + value[i];
 		}
 		else a = 0;
 		h.push_back(a);
 	}
 	a = h.front();
-	for (auto it = h.begin(); it != h.end(); ++it) if (a < *it) a = *it;
+	for (auto it = h.begin(); it != h.end(); ++it) { if (a < *it) a = *it; }
 	head[n] = a;
 	return head;
 }
@@ -27,7 +27,7 @@ int main() {
 	std::cout << "Введите вес элементов по индексам: ";
 	std::cout << std::endl;
 	for (int i = 0; i < n; ++i) {
-		std::cout << i+1 << ": ";
+		std::cout << i + 1 << ": ";
 		std::cin >> z;
 		weight.push_back(z);
 	}
@@ -35,7 +35,7 @@ int main() {
 	std::cout << "Введите вес элементов по индексам: ";
 	std::cout << std::endl;
 	for (int i = 0; i < n; ++i) {
-		std::cout << i+1 << ": ";
+		std::cout << i + 1 << ": ";
 		std::cin >> z;
 		value.push_back(z);
 	}
@@ -44,13 +44,7 @@ int main() {
 	std::cin >> z;
 	++z;
 	std::cout << std::endl;
-	for (int i = 0; i < z; ++i) {
-		head.push_back(0);
-	}
-	for (int i = 1; i < z; ++i) {
-		head = Rykzak(i, weight, value, head, n);
-	}
-	for (int i = 1; i < z; ++i) {
-		std::cout << i << " у.в. - " << head[i] << '\n';
-	}
+	for (int i = 0; i < z; ++i) { head.push_back(0); }
+	for (int i = 1; i < z; ++i) { head = Rykzak(i, weight, value, head, n); }
+	for (int i = 1; i < z; ++i) { std::cout << i << " у.в. - " << head[i] << '\n'; }
 }
